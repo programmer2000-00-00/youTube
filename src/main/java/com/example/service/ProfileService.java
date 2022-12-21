d1c238dbe60a80fa2b6362b3737837d68eb5b3c2
Doniyor <doniyortoshpulatov25@gmail.com>
2022-12-21 20:14:44 +0500

    security config

---
+++ src/main/java/com/example/service/ProfileService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import com.example.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProfileService {
+    @Autowired
+    private ProfileRepository profileRepository;
+
+
+}
