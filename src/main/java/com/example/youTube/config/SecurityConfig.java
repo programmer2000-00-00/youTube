b658f27739e7a6e6229cda5cf3c8c34efe65e5b0
Doniyor <doniyortoshpulatov25@gmail.com>
2022-12-21 15:20:44 +0500

    mazgiiiiiiiiiii

--- src/main/java/com/example/youTube/config/SecurityConfig.java
+++ src/main/java/com/example/youTube/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.youTube.config;
 public class SecurityConfig {
     public  Integer add(){
         System.out.println("Yukladim Mazgii");
+        System.out.println("Gitingzg katta mazgii");
         return  null;
     }
 }
